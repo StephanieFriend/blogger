c3eba054a2cf21710e45d6ccf387588beeb919ba
andrewetobin <andrewetobin@gmail.com>
2018-08-28 07:35:17 -0600

    adds flash notice to destroy and create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,11 +15,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     Article.destroy(params[:id])
+
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
   end
 
--- spec/features/user_creates_a_new_article_spec.rb
+++ spec/features/user_creates_a_new_article_spec.rb
@@ -4,17 +4,19 @@ describe "user creates a new article" do
   describe "they link from the articles index" do
     describe "they fill in a title and body" do
       it "creates a new article" do
+        new_title = "New Title!"
         visit articles_path
         click_link "Create a New Article"
 
         expect(current_path).to eq(new_article_path)
 
-        fill_in "article[title]", with: "New Title!"
+        fill_in "article[title]", with: new_title
         fill_in "article[body]",  with: "New Body!"
         click_on "Create Article"
 
         expect(page).to have_content("New Title!")
         expect(page).to have_content("New Body!")
+        expect(page).to have_content("Article '#{new_title}' created!")
       end
     end
   end
