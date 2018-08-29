98af97bcbde9bdf99cbfb17ede5a35eed32f4284
andrewetobin <andrewetobin@gmail.com>
2018-08-29 07:19:00 -0600

    adds comments to article show page

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -5,12 +5,19 @@ describe 'user sees one article' do
     it "shows the one article" do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
       article_2 = Article.create!(title: "Title 2", body: "Body 2")
-      visit '/articles'
+      comment_1 = article_1.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article_1.comments.create(author_name: "You", body: "So much to say")
+
+      visit articles_path
 
       click_link "Title 1"
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
       expect(page).to_not have_content(article_2.title)
       expect(page).to_not have_content(article_2.body)
     end
