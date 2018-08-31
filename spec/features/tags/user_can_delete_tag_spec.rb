ae03f58424d67b7a2e139b4aa90cc589a87231f7
andrewetobin <andrewetobin@gmail.com>
2018-08-31 08:00:25 -0600

    dependent destroy on taggings model

--- spec/features/tags/user_can_delete_tag_spec.rb
+++ spec/features/tags/user_can_delete_tag_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe 'user visits tag index page' do
+describe 'user visits tag show page' do
   it "can delete a tag" do
     article = Article.create!(title: "New Title", body: "New Body")
       tag_1 = article.tags.create!(name: "Name")
-      tag_ = article.tags.create!(name: "Subaru")
+      tag_2 = article.tags.create!(name: "Subaru")
 
     visit tag_path(tag_1)
-    click_on "Delete"
 
+    click_on "Delete"
+    
     expect(current_path).to eq(tags_path)
-    expect(page).to_not have_contect(tag_1.name)
-    expect(page).to have_contect(tag_2.name)
+    expect(page).to have_content(tag_2.name)
   end
 end
